db5ee0f9ffdb00ac86ce8f2848ee60079eae1004
Joshua Rountree <joshua@swodev.com>
2014-03-11 16:19:28 -0400

    fixes

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -70,7 +70,7 @@ Cache::config('default', array('engine' => 'File'));
  *
  */
 CakePlugin::load('Composer', array('bootstrap' => true));
-
+CakePlugin::load('Migrations');
 
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter. By default CakePHP bundles two filters:
